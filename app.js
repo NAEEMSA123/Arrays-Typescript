adec8a7f1495e3f2ef66c4a8cb7a5e5c050d67c5
“Naeem <sarimali245678@gmail.com>
2024-05-23 12:28:37 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+"use strict";
+//"Assignmet Arrays"
+Object.defineProperty(exports, "__esModule", { value: true });
+//Question 1;
+// let friuts = ["apple","banana","mango","orange"];
+// console.log(friuts);
+//Question 2;
+// let numbers = [10, 20, 30, 40];
+// console.log(numbers[3]);
+// Question 3;
+// let friuts = ["Apple", "Mango", "Strawberry", "Orange", "Banana"];
+// console.log(friuts[3]);
+//Question 4 
+// let numbers = [10, 20, 30, 40, 50];
+// numbers[1] = 25;
+// console.log(numbers);
+//Question 5
+// let friut = ['Apple', 'Mango', 'Orange'];
+// friut.push("grape");
+// console.log(friut);
+//Question 6
+// let friuts = ['Apple', 'Orange', 'Strawberry', 'Mango'];
+// friuts.pop();
+// console.log(friuts);
+//Question 7 
+// let friuts = ['Apple', 'Orange', 'Strawberry', 'Mango'];
+// friuts.shift();
+// console.log(friuts)
+//Question 8
+// let named = ['Apple', 'Orange', 'Strawberry', 'Mango'];
+// named.push('KIWI');
+// console.log(named);
+// Question 9;
+// let friuts = ['Apple', 'Orange', 'Strawberry', 'Mango'];
+// friuts.splice(1,2);
+// console.log(friuts);
+// Question 10;
+// let friuts = ['Apple', 'Orange', 'Strawberry', 'Mango'];
+// friuts.splice(2,2 ,"pineapple", "pear");
+// console.log(friuts);
+// Question 11;
+let fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry'];
+let citrusFruits = fruits.splice(0, 2);
+console.log(citrusFruits); // ['apple', 'banana']
+// Question 12;
+// let fruits = ['apple', 'banana', 'cherry', 'Orange', 'Strawberry'];
+// let lastTwoFruits = fruits.slice(-2);
+// // Output the result
+// console.log(lastTwoFruits); 
